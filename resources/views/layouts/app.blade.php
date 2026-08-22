b704b61d0b885578fc7862b2f66b611b18a545ea
delu2ted <183396016+delu2ted@users.noreply.github.com>
2026-08-23 01:22:22 +0200

    Updates: 2026-08-22T23:22:20.309Z

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
